ad9cb10e3561be04159696b9d3798fffcc54df25
Dani Camba <Camba3D@gmail.com>
2023-08-24 15:41:37 +0200

    minor changes + removed prettier in favor of eslint --fix

--- src/consts/Card.js
+++
@@ -1,4 +0,0 @@
-export const PLACEHOLDER_SCALE = 10
-export const PLACEHOLDER_W = 3 * PLACEHOLDER_SCALE
-export const PLACEHOLDER_H = 3.5 * PLACEHOLDER_SCALE
-export const PLACEHOLDER_IMG = `https://placehold.co/${PLACEHOLDER_W}x${PLACEHOLDER_H}/F84/FEC?font=roboto&text=.`
