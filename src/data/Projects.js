0d4cfa3f80f364709d5756ac0373d9b621cb7575
Camba3D <Camba3D@gmail.com>
2023-08-27 19:44:18 +0200

    project page with getstaticpaths

---
+++ src/data/Projects.js
@@ -0,0 +1,43 @@
+const placeholderImg = "/PlaceHolder.svg";
+const placeholderDesc = "Awesome Project is a placeholder description for an innovative, well-designed, scalable project that has the potential to make a positive impact on the world. The project solves a real problem that affects a large number of people, is designed to be easy to use and accessible to everyone, and can be scaled to accommodate a large number of users. The team behind Awesome Project is confident that it will be a success, and they are excited to see what the future holds for it."
+
+export const Projects = [
+
+	{
+		id: "awes-proj-1",
+		title: "Awesome Project #1",
+		img: placeholderImg,
+		desc: placeholderDesc
+	},
+	{
+		id: "awes-proj-2",
+		title: "Awesome Project #2",
+		img: placeholderImg,
+		desc: placeholderDesc
+	},
+	{
+		id: "awes-proj-3",
+		title: "Awesome Project #3",
+		img: placeholderImg,
+		desc: placeholderDesc
+	},
+	{
+		id: "cool-proj-1",
+		title: "Cool Project #1",
+		img: placeholderImg,
+		desc: placeholderDesc
+	},
+	{
+		id: "cool-proj-2",
+		title: "Cool Project #2",
+		img: placeholderImg,
+		desc: placeholderDesc
+	},
+	{
+		id: "cool-proj-3",
+		title: "Cool Project #3",
+		img: placeholderImg,
+		desc: placeholderDesc
+	},
+
+]
